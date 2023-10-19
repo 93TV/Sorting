e352d753832793cd15e944c7708cbb71fbc89c02
tuprec <thibe.provost@student.odisee.com>
2023-10-19 11:08:49 +0200

    insertion object

---
+++ src/main/java/InsesrtionSortEindwerken.java
@@ -0,0 +1,29 @@
+/**
+ * Sorting : InsesrtionSort
+ *
+ * @author viaen
+ * @version 19/10/2023
+ */
+public class InsesrtionSortEindwerken implements ISortObject {
+    @Override
+    public void sort(Comparable[] array) {
+        int inputLength = array.length;
+
+        for (int i = 1; i < inputLength; i++) {
+            Comparable currentValue = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].compareTo(currentValue)<0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = currentValue;
+        }
+    }
+
+    @Override
+    public void printArray(Comparable[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
--- src/main/java/MainSortObjectTest.java
+++ src/main/java/MainSortObjectTest.java
@@ -10,7 +10,7 @@ public class MainSortObjectTest {
     public static void main(String[] args) {
         EindwerkenArray arrEindwerken = new EindwerkenArray();
         BubbleSortEindwerken bubbleSort = new BubbleSortEindwerken();
-
+        InsesrtionSortEindwerken insertEind = new InsesrtionSortEindwerken();
 
         try {
             arrEindwerken.leesBestand();
@@ -18,10 +18,16 @@ public class MainSortObjectTest {
             throw new RuntimeException(e);
         }
         double start = System.currentTimeMillis();
+
         System.out.println("before");
-        bubbleSort.sort(arrEindwerken.getEindwerken());
+//        bubbleSort.printArray(arrEindwerken.getEindwerken());
+//        bubbleSort.sort(arrEindwerken.getEindwerken());
+            insertEind.sort(arrEindwerken.getEindwerken());
+
+
         System.out.println("After");
-        //bubbleSort.printArray(arrEindwerken.getEindwerken());
+//        bubbleSort.printArray(arrEindwerken.getEindwerken());
+
         double end = System.currentTimeMillis();
         System.out.println(end-start);
     }
